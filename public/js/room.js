bad847a1364bd5946bba1a250f4a5dbfedce99d0
Iulian Rusu <iulian.rusu999@gmail.com>
2021-05-24 13:26:29 +0300

    Refactor deleting room

--- public/js/room.js
+++ public/js/room.js
@@ -11,15 +11,16 @@ function deleteRoom(name) {
     };
 
     xhttp.open("DELETE", `/room`, true);
-    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(`name=${name}`);
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    const json = JSON.stringify({ name: name });
+    xhttp.send(json);
 }
 
 function removeRoomFromTable(name) {
     const roomsParent = document.getElementById("my-rooms-body");
     const rooms = roomsParent.childNodes;
     for (let i = 0; i < rooms.length; ++i) {
-        if(rooms[i].id == name) {
+        if (rooms[i].id == name) {
             roomsParent.removeChild(rooms[i]);
         }
     }
